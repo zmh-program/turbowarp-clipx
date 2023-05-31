0b482af8dbfe5c7055ca7142bbdb6dc940cabecd
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-31 17:25:08 +0800

    update cache

--- src/utils.ts
+++ src/utils.ts
@@ -1,74 +1,163 @@
-export function cleanObject (target: Record<string, any>): object {
+export function cleanObject(target: Record<string, any>): object {
   for (const k in target) {
     if (target[k] === undefined) delete target[k]
   }
   return target
 }
 
-export function notification (content: string): void {
+export function truncate(str: string, len: number = 10): string {
+    return (str.length > (len)) ? `${str.slice(0, (len))}...` : str;
+}
+
+export function notification(content: string): void {
   // @ts-expect-error
   top.mdui.snackbar(content)
 }
 
 class Cache {
-  protected caches: Record<string, Record<string, any>>;
-  constructor() {
+  /**
+   * Cache Manager.
+   *
+   * Structure:
+   * e.g.
+   *
+   * opcode
+   *  - expiration (secs)
+   *  - data
+   *    - key1
+   *      - json args
+   *      - stamp
+   *    - key2
+   *    ...
+   *
+   */
+
+  protected caches: Record<
+      string, // opcode
+      {
+        data: Record<string, {
+          value: any,
+          stamp: number,
+        }>,
+        expiration: number,
+      }
+    >;
+  public secs: number;
+  public debug: boolean;
+
+  public constructor(secs ?: number, debug ?: boolean) {
     this.caches = {};
-    this.uptime();
+    this.secs = secs || 60;
+    this.debug = debug || false;
+    this.start();
   }
 
-  get(key: string): undefined | any {
-    const value = this.caches[key];
-    if (this.exist(key)) {
-      return JSON.parse(value.value);
+  public register(tag: string, expiration: number) {
+    this.caches[tag] = {
+      data: {},
+      expiration: expiration,
     }
   }
 
-  set(key: string, value: any, expiration: number): void {
-    this.caches[key] = {
+  static is_expired(stamp: number, expiration: number): boolean {
+      return expiration === 0 ? false : (new Date().getTime() / 1000) > (stamp + expiration);
+  }
+
+  public is_registered(tag: string): boolean {
+    return this.caches[tag] !== undefined;
+  }
+
+  public exist(tag: string, key: string): boolean {
+    if (!this.is_registered(tag)) return false;
+    return this.caches[tag].data[key] !== undefined;
+  }
+
+  public is_available(tag: string, key: string): boolean {
+    if (!this.exist(tag, key)) return false;
+    const { data, expiration} = this.caches[tag];
+    return !Cache.is_expired(data[key].stamp, expiration);
+  }
+
+  public get(tag: string, key: string): undefined | any {
+    if (!this.is_available(tag, key)) return undefined;
+    return JSON.parse(this.caches[tag].data[key].value);
+  }
+
+  public set(tag: string, key: string, value: any, stamp: number): void {
+    if (!this.is_registered(tag)) return;
+    this.caches[tag].data[key] = {
       value: JSON.stringify(value),
-      expiration: (new Date().getTime() / 1000) + expiration,
+      stamp: stamp,
     }
   }
 
-  exist(key: string): boolean {
-    const value = this.caches[key];
-    return (!!value) && (value.expiration > (new Date().getTime() / 1000));
+  remove(tag: string, key: string): boolean {
+    if (!this.exist(tag, key)) return false;
+    delete this.caches[tag].data[key];
+    return true;
   }
 
-  remove(key: string): boolean {
-    return delete this.caches[key];
-  }
-
-  uptime(): void {
+  start(): void {
     const _this = this;
-    setInterval(function (){
-      let n: number = 0;
-      for (const key in _this.caches) {
-        if (_this.caches[key].expiration < (new Date().getTime() / 1000)) {
-          _this.remove(key); n++;
+    setInterval(() => {
+      let count: number = 0;
+      for (const tag in _this.caches) {
+        const { data, expiration } = _this.caches[tag];
+        for (const key in data) {
+          if (Cache.is_expired(data[key].stamp, expiration)) {
+            count ++;
+            delete data[key];
+          }
         }
       }
-    }, 60000);
+      if (count && _this.debug) console.debug(`[Cache] ${count} items expired.`);
+    }, this.secs * 1000);
   }
 
-  cache_async(name: string, func: (...params: any[]) => Promise<any>, expiration: number): (...params: any[]) => Promise<any> {
+  public cache_async(tag: string, func: (...params: any[]) => Promise<any>): (...params: any[]) => Promise<any> {
     /**
      * Async Function Cache.
      */
+    const _this = this;
+    return async function(...params: any[]) {
+        const key = JSON.stringify(params);
+        if (_this.is_available(tag, key)) {
+            if (_this.debug) console.log(`[Cache] ${tag}(${truncate(key)}) hit.`);
+            return _this.get(tag, key);
+        }
+        const stamp = new Date().getTime() / 1000;
+        const result = await func(...params);
+        _this.set(tag, key, result, stamp);
+        return result;
+    }
+  }
 
-    const _this: Cache = this;
-    return async function (...params : any[]) {
-      const key: string = name + params.toString();
-      if (_this.exist(key)) {
-        return _this.get(key);
-      } else {
-        const response: any = await func(...params);
-        _this.set(key, response, expiration);
-        return response;
-      }
+  public cache_sync(tag: string, func: (...params: any[]) => any): (...params: any[]) => any {
+    /**
+     * Sync Function Cache.
+     */
+    const _this = this;
+    return function(...params: any[]) {
+        const key = JSON.stringify(params);
+        if (_this.is_available(tag, key)) {
+            if (_this.debug) console.log(`[Cache] ${tag}(${truncate(key)}) hit.`);
+            return _this.get(tag, key);
+        }
+        const stamp = new Date().getTime() / 1000;
+        const result = func(...params);
+        _this.set(tag, key, result, stamp);
+        return result;
+    }
+  }
+
+  public cache(tag: string, func: (...params: any[]) => any): (...params: any[]) => any {
+    /**
+     * Function Cache.
+     */
+    if (func.constructor.name === 'AsyncFunction') {
+        return this.cache_async(tag, func);
+    } else {
+        return this.cache_sync(tag, func);
     }
   }
 }
-
-export const cache = new Cache();
